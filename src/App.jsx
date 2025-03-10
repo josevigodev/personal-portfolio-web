f68c0069381aceea06510b1d53f304ea4fb9052b
Jose Vigo <josevigodev@gmail.com>
2025-03-10 10:48:45 -0700

    adding separate users file

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,6 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
-
-const users = [
-  {
-    userName: 'josevigodev',
-    name: 'Jose Vigo',
-    isFollowing: true
-  },
-  {
-    userName: 'midudev',
-    name: 'Miguel Angel Duran',
-    isFollowing: false
-  },
-  {
-    userName: 'pheralb',
-    name: 'Pedro Pica',
-    isFollowing: false
-  },
-  {
-    userName: 'emily',
-    name: 'Emily Reyes',
-    isFollowing: false
-  }
-];
+import { users } from './users'
 
 export function App() {
   return (
---
+++ src/users.js
@@ -0,0 +1,27 @@
+export const users = [
+  {
+    userName: 'josevigodev',
+    name: 'Jose Vigo',
+    isFollowing: true
+  },
+  {
+    userName: 'midudev',
+    name: 'Miguel Angel Duran',
+    isFollowing: false
+  },
+  {
+    userName: 'pheralb',
+    name: 'Pedro Pica',
+    isFollowing: false
+  },
+  {
+    userName: 'emily',
+    name: 'Emily Reyes',
+    isFollowing: false
+  },
+  {
+    userName: 'lola',
+    name: 'Lola Mento',
+    isFollowing: true
+  }
+];

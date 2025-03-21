0b97fcb01249f4fccd04b8d75e67b8c951b53279
Jose Vigo <josevigodev@gmail.com>
2025-03-21 10:27:39 -0700

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 const FollowMouse = () => {
-  const [ enabled, setEnabled] = useState(false);
-  const [ position, setPosition ] = useState({x: 0, y: 0})
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
 
   // pointer move
   useEffect(() => {
     console.log('effect', { enabled });
 
-    const handleMove = event => {
+    const handleMove = (event) => {
       const { clientX, clientY } = event;
-      setPosition({ x: clientX, y: clientY});
-    }
+      setPosition({ x: clientX, y: clientY });
+    };
 
     if (enabled) {
       window.addEventListener('pointermove', handleMove);
@@ -19,9 +19,9 @@ const FollowMouse = () => {
 
     // cleanup
     return () => {
-      console.log('cleanup')
-      window.removeEventListener('pointermove', handleMove)
-    }
+      console.log('cleanup');
+      window.removeEventListener('pointermove', handleMove);
+    };
   }, [enabled]);
 
   // change body class name
@@ -32,36 +32,37 @@ const FollowMouse = () => {
 
     return () => {
       document.body.classList.remove('no-cursor');
-    }
-  }, [enabled])
+    };
+  }, [enabled]);
 
   return (
     <>
-      <div style={{
-        position: 'absolute',
-        backgroundColor: '#09f',
-        borderRadius: '50%',
-        opacity: '0.8',
-        pointerEvents: 'none',
-        left: '-20px',
-        top: '-20px',
-        width: '40px',
-        height: '40px',
-        transition: 'transform .1s ease',
-        transform: `translate(${position.x}px, ${position.y}px)`
-      }}
+      <div
+        style={{
+          position: 'absolute',
+          backgroundColor: '#09f',
+          borderRadius: '50%',
+          opacity: '0.8',
+          pointerEvents: 'none',
+          left: '-20px',
+          top: '-20px',
+          width: '40px',
+          height: '40px',
+          transition: 'transform .1s ease',
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
       />
       <button onClick={() => setEnabled(!enabled)}>
         Turn {enabled ? 'off' : 'on'} follow cursor
       </button>
     </>
-  )
-}
+  );
+};
 
 export function App() {
   return (
     <main>
-      <FollowMouse/>
+      <FollowMouse />
     </main>
-  )
+  );
 }

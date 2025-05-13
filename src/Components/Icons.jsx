81bba4837eb0ddd3fb67ff22f4c1d2e86e251709
Jose Vigo <josevigodev@gmail.com>
2025-05-12 17:41:31 -0700

    add social links and gmail

---
+++ src/Components/Icons.jsx
@@ -0,0 +1,200 @@
+export function CopyIcon() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='20'
+      height='20'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      stroke-width='2'
+      stroke-linecap='round'
+      stroke-linejoin='round'
+      class='icon icon-tabler icons-tabler-outline icon-tabler-copy'
+    >
+      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+      <path d='M7 7m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z' />
+      <path d='M4.012 16.737a2.005 2.005 0 0 1 -1.012 -1.737v-10c0 -1.1 .9 -2 2 -2h10c.75 0 1.158 .385 1.5 1' />
+    </svg>
+  );
+}
+
+export function SendIcon() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='20'
+      height='20'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      stroke-width='2'
+      stroke-linecap='round'
+      stroke-linejoin='round'
+      class='icon icon-tabler icons-tabler-outline icon-tabler-brand-telegram'
+    >
+      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+      <path d='M15 10l-4 4l6 6l4 -16l-18 7l4 2l2 6l3 -4' />
+    </svg>
+  );
+}
+
+export function GitIcon() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      stroke-width='2'
+      stroke-linecap='round'
+      stroke-linejoin='round'
+      class='icon icon-tabler icons-tabler-outline icon-tabler-brand-git'
+    >
+      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+      <path d='M16 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0' />
+      <path d='M12 8m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0' />
+      <path d='M12 16m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0' />
+      <path d='M12 15v-6' />
+      <path d='M15 11l-2 -2' />
+      <path d='M11 7l-1.9 -1.9' />
+      <path d='M13.446 2.6l7.955 7.954a2.045 2.045 0 0 1 0 2.892l-7.955 7.955a2.045 2.045 0 0 1 -2.892 0l-7.955 -7.955a2.045 2.045 0 0 1 0 -2.892l7.955 -7.955a2.045 2.045 0 0 1 2.892 0z' />
+    </svg>
+  );
+}
+
+export function GithubIcon() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      stroke-width='2'
+      stroke-linecap='round'
+      stroke-linejoin='round'
+      class='icon icon-tabler icons-tabler-outline icon-tabler-brand-github'
+    >
+      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+      <path d='M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5' />
+    </svg>
+  );
+}
+
+export function LinkedinIcon() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      stroke-width='2'
+      stroke-linecap='round'
+      stroke-linejoin='round'
+      class='icon icon-tabler icons-tabler-outline icon-tabler-brand-linkedin'
+    >
+      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+      <path d='M8 11v5' />
+      <path d='M8 8v.01' />
+      <path d='M12 16v-5' />
+      <path d='M16 16v-3a2 2 0 1 0 -4 0' />
+      <path d='M3 7a4 4 0 0 1 4 -4h10a4 4 0 0 1 4 4v10a4 4 0 0 1 -4 4h-10a4 4 0 0 1 -4 -4z' />
+    </svg>
+  );
+}
+
+export function HTMLIcon() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      stroke-width='2'
+      stroke-linecap='round'
+      stroke-linejoin='round'
+      class='icon icon-tabler icons-tabler-outline icon-tabler-brand-html5'
+    >
+      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+      <path d='M20 4l-2 14.5l-6 2l-6 -2l-2 -14.5z' />
+      <path d='M15.5 8h-7l.5 4h6l-.5 3.5l-2.5 .75l-2.5 -.75l-.1 -.5' />
+    </svg>
+  );
+}
+
+export function CSSIcon() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      stroke-width='2'
+      stroke-linecap='round'
+      stroke-linejoin='round'
+      class='icon icon-tabler icons-tabler-outline icon-tabler-brand-css3'
+    >
+      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+      <path d='M20 4l-2 14.5l-6 2l-6 -2l-2 -14.5z' />
+      <path d='M8.5 8h7l-4.5 4h4l-.5 3.5l-2.5 .75l-2.5 -.75l-.1 -.5' />
+    </svg>
+  );
+}
+
+export function JavascriptIcon() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      stroke-width='2'
+      stroke-linecap='round'
+      stroke-linejoin='round'
+      class='icon icon-tabler icons-tabler-outline icon-tabler-brand-javascript'
+    >
+      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+      <path d='M20 4l-2 14.5l-6 2l-6 -2l-2 -14.5z' />
+      <path d='M7.5 8h3v8l-2 -1' />
+      <path d='M16.5 8h-2.5a.5 .5 0 0 0 -.5 .5v3a.5 .5 0 0 0 .5 .5h1.423a.5 .5 0 0 1 .495 .57l-.418 2.93l-2 .5' />
+    </svg>
+  );
+}
+
+export function ReactIcon() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      stroke-width='2'
+      stroke-linecap='round'
+      stroke-linejoin='round'
+      class='icon icon-tabler icons-tabler-outline icon-tabler-brand-react'
+    >
+      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+      <path d='M6.306 8.711c-2.602 .723 -4.306 1.926 -4.306 3.289c0 2.21 4.477 4 10 4c.773 0 1.526 -.035 2.248 -.102' />
+      <path d='M17.692 15.289c2.603 -.722 4.308 -1.926 4.308 -3.289c0 -2.21 -4.477 -4 -10 -4c-.773 0 -1.526 .035 -2.25 .102' />
+      <path d='M6.305 15.287c-.676 2.615 -.485 4.693 .695 5.373c1.913 1.105 5.703 -1.877 8.464 -6.66c.387 -.67 .733 -1.339 1.036 -2' />
+      <path d='M17.694 8.716c.677 -2.616 .487 -4.696 -.694 -5.376c-1.913 -1.105 -5.703 1.877 -8.464 6.66c-.387 .67 -.733 1.34 -1.037 2' />
+      <path d='M12 5.424c-1.925 -1.892 -3.82 -2.766 -5 -2.084c-1.913 1.104 -1.226 5.877 1.536 10.66c.386 .67 .793 1.304 1.212 1.896' />
+      <path d='M12 18.574c1.926 1.893 3.821 2.768 5 2.086c1.913 -1.104 1.226 -5.877 -1.536 -10.66c-.375 -.65 -.78 -1.283 -1.212 -1.897' />
+      <path d='M11.5 12.866a1 1 0 1 0 1 -1.732a1 1 0 0 0 -1 1.732z' />
+    </svg>
+  );
+}
--- src/Components/OpeningHeader.jsx
+++ src/Components/OpeningHeader.jsx
@@ -1,16 +1,52 @@
 import { ResponsiveImage } from './ResponsiveImage';
 import '../styles/OpeningHeader.css';
+import { CopyIcon, GithubIcon, LinkedinIcon, SendIcon } from './Icons';
 
 export function OpeningHeader() {
+  const handleCopy = () => {
+    navigator.clipboard.writeText('josevigodev@gmail.com');
+  };
+
   return (
     <div className='blur'>
       <header className='width-padding-container full-height flex-container opening-header'>
         <div className='flex-column-container principal-title-container'>
-          <small>Hola</small>
-          <h1>
-            Soy <span className='animated-gradient-text'>Jose Vigo</span>
-          </h1>
+          <small>Hola soy</small>
+          <h1 className='animated-gradient-text'>Jose Vigo</h1>
           <h2>Desarrollador Web Frontend</h2>
+          <div className='flex-row-container contact-info'>
+            <p>josevigodev@gmail.com</p>
+            <button onClick={handleCopy}>
+              <CopyIcon />
+            </button>
+            <a href='mailto:josevigodev@gmail.com'>
+              <SendIcon />
+            </a>
+          </div>
+          <div className='flex-row-container social-container'>
+            <a
+              className='flex-row-container social-link'
+              href='https://www.linkedin.com/in/jose-vigo-a9a728365'
+              target='_blank'
+              rel='noopener noreferrer nofollow'
+            >
+              <span>
+                <LinkedinIcon />
+              </span>
+              <span>Linkedin</span>
+            </a>
+            <a
+              className='flex-row-container social-link'
+              href='https://github.com/josevigodev'
+              target='_blank'
+              rel='noopener noreferrer nofollow'
+            >
+              <span>
+                <GithubIcon />
+              </span>
+              <span>Github</span>
+            </a>
+          </div>
         </div>
         <div className='presentation-img-container'>
           <ResponsiveImage />
